83a0d8e17fc8b9d0c1e0210299e1c47542c833bf
elsawad <60275541+elsawad@users.noreply.github.com>
2026-05-30 23:23:53 -0300

    Reorder ABNF character class externs

--- include/ABNF.h
+++ include/ABNF.h
@@ -4,9 +4,8 @@
 #include "CharacterClass.h"
 
 // ABNF basic rules rules as defined in RFC 5234 appendix B.1
-extern const CharacterClass HEXDIG;
 extern const CharacterClass DIGIT;
-extern const CharacterClass TCHAR;
+extern const CharacterClass HEXDIG;
 extern const CharacterClass VCHAR;
 extern const CharacterClass WSP;
 
